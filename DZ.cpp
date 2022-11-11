4d1443d5c76316173f43f5683fb188c9d5733bd9
ilyaLobeuko <114007539+ilyaLobeuko@users.noreply.github.com>
2022-11-12 00:02:48 +0300

    Add files via upload

---
+++ DZ.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main(){
+    setlocale(LC_ALL, "rus");
+	/*Array7.Дан массив размера N. Вывести его элементы в обратном порядке.*/
+    /*int arr[10];
+    int n;
+    cout << "Razmer massiva : ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "arr[" << i << "]" ": ";
+        cin >> arr[i];
+    }
+    cout << endl;
+    for (int i = n - 1; i >= 0; i--)
+        cout << "arr[" << i << "]" ": " << arr[i] << endl;*/
+    /*Array8 Дан целочисленный массив размера N. Вывести все содержащиеся в
+данном массиве нечетные числа в порядке возрастания их индексов, а
+также их количество K */
+    /*int n, k = 0;
+    cin >> n;
+    int arr[10];
+    for (int i = 1; i > arr[i]; i <= n) {
+        i++;
+        if (arr[i] % 2 != 0)
+        {
+            cout << endl << arr[i];
+            k++;
+        }
+    }cout << endl << k;*/
+
+    //а дальше сдался -_- и я не уерен в правильности 2 задания даже -_-
+
+
+
+
+}
